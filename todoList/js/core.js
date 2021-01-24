9bec04cda94a82623cd9f2dd5858cea59d8bc53c
Dieudonne Twahirwa <twahirwa25@gmail.com>
2021-01-25 00:17:43 +0100

    update

--- todoList/js/core.js
+++ todoList/js/core.js
@@ -13,7 +13,11 @@ fields.forEach((field) => {
   field.addEventListener(
     "blur",
     () => {
-      validateField(field);
+      if (validateField(field)) {
+        if (field.type == "date") {
+          validateDate(field);
+        }
+      }
     },
     false
   );
@@ -54,15 +58,41 @@ taskForm.addEventListener(
       if (!validateField(field)) {
         valid = false;
       }
+
+      // Check if the field is a date
+      if (field.type == "date") {
+        if (!validateDate(field)) {
+          valid = false;
+        }
+      }
     });
 
     // if valid submit
     if (valid) {
-      e.target.submit();
+      //e.target.submit();
     }
   },
   false
 );
+
+//Function that check the validity of the date
+function validateDate(field) {
+  let now = Date.now();
+  let deadline = field.value;
+
+  // Check if the date entred is in the past
+  if (deadline > now) {
+    return true;
+  } else {
+    // Add "invalid" class for the invalid field entry
+    field.classList.add("invalid");
+    field.previousElementSibling.insertAdjacentHTML(
+      "beforeend",
+      '<span class="msg">Please enter a valid date</span>'
+    );
+    return false;
+  }
+}
 
 // Function that check if a field entry is valid
 function validateField(field) {
