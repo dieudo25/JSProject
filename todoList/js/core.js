ba24db90efcce0d111ff85cbb814298563d764c2
Dieudonne Twahirwa <twahirwa25@gmail.com>
2021-01-24 23:38:09 +0100

    update

--- todoList/js/core.js
+++ todoList/js/core.js
@@ -1,13 +1,98 @@
 // Get the button new
-let newTaskButton = document.querySelector("#new_task");
+let newTaskButton = document.querySelector("#new_task_button");
+let fields = document.querySelectorAll("input[required], textarea[required]");
 
+fields.forEach((field) => {
+  field.addEventListener(
+    "focus",
+    () => {
+      resetField(field);
+    },
+    false
+  );
+  field.addEventListener(
+    "blur",
+    () => {
+      validateField(field);
+    },
+    false
+  );
+});
 // Cretate the event when the button new is clicked
-newTaskButton.addEventListener("click", displayNewTaskForm, false);
+newTaskButton.addEventListener(
+  "click",
+  (e) => {
+    // Get the form for creating a new task
+    let newTaskForm = document.querySelector("#new_task");
 
-function displayNewTaskForm(e) {
-  // Get the form for creating a new task
-  let newTaskForm = document.querySelector("#new_task_form");
+    // Toggle the class hide to the newTaskForm
+    newTaskForm.classList.toggle("hide");
+  },
+  false
+);
 
-  // Toggle the class hide to the newTaskForm
-  newTaskForm.classList.toggle("hide");
+// Get the new task form
+let taskForm = document.querySelector("#new_task_form");
+
+// Event : Submit new task form
+taskForm.addEventListener(
+  "submit",
+  (e) => {
+    // prevent default behaviour of the form
+    e.preventDefault();
+
+    // Reset error message for each field of the form
+    fields.forEach((field) => {
+      resetField(field);
+    });
+
+    // Default value
+    let valid = true;
+
+    // Check if each field is valid
+    fields.forEach((field) => {
+      if (!validateField(field)) {
+        valid = false;
+      }
+    });
+
+    // if valid submit
+    if (valid) {
+      e.target.submit();
+    }
+  },
+  false
+);
+
+// Function that check if a field entry is valid
+function validateField(field) {
+  if (field.checkValidity()) {
+    return true;
+  } else {
+    // Add "invalid" class for the invalid field entry
+    field.classList.add("invalid");
+    field.previousElementSibling.insertAdjacentHTML(
+      "beforeend",
+      `<span class="msg">${field.validationMessage}</span>`
+    );
+    return false;
+  }
 }
+
+// Function that remove the previous invalid entry
+function resetField(field) {
+  let fieldLabel = field.previousElementSibling;
+  field.classList.remove("invalid");
+
+  while (fieldLabel.firstElementChild) {
+    fieldLabel.removeChild(fieldLabel.firstElementChild);
+  }
+
+  field.valid = true;
+}
+
+/* let task = {
+  "name":,
+  "deadline":,
+  "description":,
+} */
