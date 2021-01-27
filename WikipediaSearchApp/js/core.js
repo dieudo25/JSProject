41d1892de3d5d9bb7ce6869f89d22ebe0f2620b3
Dieudonne Twahirwa <twahirwa25@gmail.com>
2021-01-27 04:58:54 +0100

    update wiki search

--- WikipediaSearchApp/js/core.js
+++ WikipediaSearchApp/js/core.js
@@ -1,7 +1,14 @@
+// Get the form element
 const form = document.querySelector(".js-search-form");
+// Get a reference to the `.js-search-results` element
+const searchResults = document.querySelector(".js-search-results");
+// Get the spinner element
+const spinner = document.querySelector(".js-spinner");
 
+// Listen to the event when the form is submited
 form.addEventListener("submit", handleSubmit, false);
 
+//  Function that will handle the submission of the form
 async function handleSubmit(event) {
   // prevent page from reloading when form is submitted
   event.preventDefault();
@@ -9,14 +16,26 @@ async function handleSubmit(event) {
   const inputValue = document.querySelector(".js-search-input").value;
   // Remove the whitespace and tabs from the input
   const searchQuery = inputValue.trim();
-
+  // Clear the previous results
+  searchResults.innerHTML = "";
+  // Remove hidden class from spinner element
+  spinner.classList.remove("hidden");
   try {
     // Get the results from the API
     const results = await searchWikipedia(searchQuery);
-    console.log(results);
+    // If there is no results
+    if (results.query.searchinfo.totalhits === 0) {
+      alert("No results found. Try different keywords");
+      return;
+    }
+    // Display the results of the search by the aid of the function
+    displayResults(results);
   } catch (err) {
     console.log(err);
     alert("Failed to search Wikipedia!");
+  } finally {
+    // Add hidden class to spinner element
+    spinner.classList.add("hidden");
   }
 }
 
@@ -58,4 +77,24 @@ async function searchWikipedia(searchQuery) {
   // If the fetch is succeful format the response in json
   const json = response.json();
   return json;
+}
+
+function displayResults(results) {
+  // Iterate over the `search` array. Each nested object in the array can be
+  // accessed through the `result` parameter
+  results.query.search.forEach((result) => {
+    const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+    // Append the search result to the DOM
+    searchResults.insertAdjacentHTML(
+      "beforeend",
+      `<div class="result-item">
+          <h3 class="result-title">
+            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+          </h3>
+          <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+          <span class="result-snippet">${result.snippet}</span><br>
+        </div>`
+    );
+  });
 }
